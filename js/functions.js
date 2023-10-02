e3f0fb8d1f77144479410094b83d8f2b49e0c545
k4noise <vestertp@gmail.com>
2023-10-02 21:39:42 +0500

    add: descriptions of functions

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,18 @@
+// Задание 1
+// Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее.
 const checkMaxLength = (value, maxLength) => value.length <= maxLength;
 
+
+// Задание 2
+// Функция для проверки, является ли строка палиндромом.
 const isPalindrome = (sentence) => {
   const normalizedSentence = sentence.replaceAll(' ', '').toLowerCase();
   const reversedSentence = normalizedSentence.split('').reverse().join('');
   return normalizedSentence === reversedSentence;
 };
 
+// Задание 3
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
 const parseNumber = (value) => {
   const number = value
     .toString()
