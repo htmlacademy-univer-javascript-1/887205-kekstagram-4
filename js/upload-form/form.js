69df8ff2a63c1a7cb0988487cd0f449d5375371a
k4noise <vestertp@gmail.com>
2023-11-26 15:30:54 +0500

    feat: image filters & scale

--- js/upload-form/form.js
+++ js/upload-form/form.js
@@ -1,12 +1,23 @@
 import { onCloseModal } from '../util.js';
 import { addValidators } from './validation.js';
+import { createEffectSlider, resetEffectSlider } from './effects.js';
+
+const Zoom = {
+  MIN: 25,
+  MAX: 100,
+  STEP: 25
+};
 
 const uploadForm = document.querySelector('.img-upload__form');
+const uploadedImageNode = document.querySelector('.img-upload__preview img');
 const uploadInputNode = document.querySelector('.img-upload__input');
 const uploadOverlayNode = document.querySelector('.img-upload__overlay');
-const closeIconNode = document.querySelector('.img-upload__cancel');
 const sendButtonNode = document.querySelector('.img-upload__submit');
+const closeIconNode = document.querySelector('.img-upload__cancel');
 
+const scaleValueNode = document.querySelector('.scale__control--value');
+const scaleUpButtonNode = document.querySelector('.scale__control--bigger');
+const scaleDownButtonNode = document.querySelector('.scale__control--smaller');
 
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
@@ -14,13 +25,25 @@ const pristine = new Pristine(uploadForm, {
   errorTextClass: 'img-upload__error'
 });
 
+const changeScale = (sign) => {
+  let scaleValue = parseFloat(scaleValueNode.value);
+  scaleValue = Math.max(Zoom.MIN, Math.min(Zoom.MAX, scaleValue + sign * Zoom.STEP));
+  scaleValueNode.value = `${scaleValue}%`;
+  uploadedImageNode.style.transform = `scale(${scaleValue / 100})`;
+};
+
+const onScaleUpButtonClick = () => changeScale(1);
+const onScaleDownButtonClick = () => changeScale(-1);
+
 const createForm = () => {
+  createEffectSlider();
   addValidators(pristine);
   uploadForm.addEventListener('input', () => {
     sendButtonNode.disabled = !pristine.validate();
   });
+  scaleUpButtonNode.addEventListener('click', onScaleUpButtonClick);
+  scaleDownButtonNode.addEventListener('click', onScaleDownButtonClick);
 };
-
 
 const onChangeUploadInput = () => {
   uploadOverlayNode.classList.remove('hidden');
@@ -32,11 +55,12 @@ const closeUploadImageForm = () => {
   document.body.classList.remove('modal-open');
   uploadForm.reset();
   pristine.reset();
+  resetEffectSlider();
 };
 
 const onDocumentKeydown = (event) => onCloseModal(event, closeUploadImageForm);
 
-const onCloseIconClick = () => closeUploadImageForm;
+const onCloseIconClick = closeUploadImageForm;
 
 uploadInputNode.addEventListener('change', onChangeUploadInput);
 closeIconNode.addEventListener('click', onCloseIconClick);
